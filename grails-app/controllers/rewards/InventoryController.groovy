f84a0b495aeb44346df6d905e05794f7b0008f9d
Mahmoud Rabie <mahmoudrabie@Mahmouds-MacBook-Pro-2.local>
2019-02-23 16:46:13 +0300

    Initial commit

---
+++ grails-app/controllers/rewards/InventoryController.groovy
@@ -0,0 +1,27 @@
+package rewards
+
+class InventoryController {
+
+    def index() {
+        render "Here is a list of everything."
+    }
+
+    def remove(){
+        render "You have one less than before."
+    }
+
+    def edit(){
+        def productName = "Breakfast Blend"
+        def sku = "BB01"
+        [product:productName , sku:sku ]
+    }
+
+    def list(){
+        /*Goto that domain called Product and give me a list of everything inside it*/
+        def allProducts = Product.list()
+        [viewAllProducts: allProducts]
+    }
+
+
+
+}
